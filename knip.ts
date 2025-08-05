bc22a5202c486230ef2cc06de224feca3e46041c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-05 18:20:01 +1000

    chore: upgrade other packages

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['src/main/index.ts', 'src/renderer/index.ts', 'src/preload/index.ts', 'vite.*.ts'],
+  entry: ['src/main/index.ts', 'src/renderer/index.ts', 'src/preload/index.ts', 'vite.*.mts'],
   project: ['src/**/*.{ts,tsx}!'],
   // TODO(psyche): these deps are somehow not recognized by knip so we need to explicitly ignore them
   ignoreDependencies: [
@@ -9,20 +9,9 @@ const config: KnipConfig = {
     '@electron-forge/plugin-fuses',
     '@electron-forge/plugin-vite',
     '@electron/fuses',
-    '@typescript-eslint/eslint-plugin',
-    '@typescript-eslint/parser',
     '@vitejs/plugin-react',
-    '@vitejs/plugin-react-swc',
-    'eslint-plugin-import',
-    'eslint-plugin-react-hooks',
-    'eslint-plugin-react-refresh',
-    'eslint-plugin-unused-imports',
-    'globals',
-    'ts-node',
     'typescript-eslint',
     'vite-plugin-eslint',
-    'vite-tsconfig-paths',
-    'vite-plugin-html',
   ],
   ignoreBinaries: [
     // This is included with @electron/forge

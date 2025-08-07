74db68ec44a1e56b47d5bc6c3dca0fd17c16d2cc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-07 16:36:05 +1000

    chore: tidy lint configs

--- knip.ts
+++ knip.ts
@@ -1,18 +1,10 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['src/main/index.ts', 'src/renderer/index.ts', 'src/preload/index.ts', 'vite.*.mts'],
+  entry: ['src/main/index.ts', 'src/renderer/index.ts', 'src/preload/index.ts', 'electron.vite.config.ts'],
   project: ['src/**/*.{ts,tsx}!'],
   // TODO(psyche): these deps are somehow not recognized by knip so we need to explicitly ignore them
-  ignoreDependencies: [
-    '@electron-forge/plugin-auto-unpack-natives',
-    '@electron-forge/plugin-fuses',
-    '@electron-forge/plugin-vite',
-    '@electron/fuses',
-    '@vitejs/plugin-react',
-    'typescript-eslint',
-    'vite-plugin-eslint',
-  ],
+  ignoreDependencies: ['@vitejs/plugin-react', 'typescript-eslint', 'vite-plugin-eslint'],
   ignoreBinaries: [
     // This is included with @electron/forge
     'electron-rebuild',
